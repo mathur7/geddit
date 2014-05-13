d8b56e5d92f5b276273d953c525a4d24b54c0792
mathur7 <mathur7@gmail.com>
2014-05-13 11:48:49 -0700

    first commit on geddit lab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geddit::Application.config.secret_key_base = 'df1a842413337d9c575ff39d0af76c4490968dc04fd91738e3485c2d90ee156fe4e4a4187ef9c8fe209b4a250c29fa84073d3d24a8e64e34ec33607035c2caac'
