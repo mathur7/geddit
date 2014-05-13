31918eab875d9827390bd507f6ff95594e64f317
mathur7 <mathur7@gmail.com>
2014-05-13 13:38:50 -0700

    added a secret key for devise, new routes to routes file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,12 @@ Geddit::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.force_ssl = true
 end
