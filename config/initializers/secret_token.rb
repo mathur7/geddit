58e1125b2f4f02c9664f4f8c7adadceff6a8e958
mathur7 <mathur7@gmail.com>
2014-05-13 12:02:25 -0700

    Hiding the secrets using a ENV hash. Forcing ssl in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Geddit::Application.config.secret_key_base = 'df1a842413337d9c575ff39d0af76c4490968dc04fd91738e3485c2d90ee156fe4e4a4187ef9c8fe209b4a250c29fa84073d3d24a8e64e34ec33607035c2caac'
+Geddit::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
